7923e11a75bb8053fea71615002105b1653b35c1
Wunmi Dabiri <oluwafumbidabiri@gmail.com>
2021-03-05 12:14:18 +0100

    fixed white space

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-100 bg-near-white pv2 ph2 overflow-scroll cf">
+    <div class="w-100 vh-100 bg-near-white overflow-hidden cf">
         <div class=" w-100   pv2 ph2 h-100  ">
             <div class="flex pa2 w-100 h-100"> 
 
@@ -9,13 +9,13 @@
 
                 <div class="fl  w-100">
                     <div class="fl pt4 w-100">
-                        <h1 class=" black sans-serif f2-l">Discover beautiful<br>African images.<br></h1>
+                        <h1 class=" black sans-serif f4 f2-l">Discover beautiful<br>African images.<br></h1>
                     </div>
 
                     <div class="fl w-80">
                         <div class="items-center ">
                             <span class="pa2 bg-white fl pointer">
-                                <i class="fas fa-search"></i>
+                                <i class="f7 fas fa-search"></i>
                             </span>
                             <input class="f7 f6-l sans-serif input-reset bn fl black-80 bg-white pa2 lh-solid w-60 " autocomplete="off" placeholder="Search high free resolution images" type="text" name="search" value="" id="search">
                         </div>
@@ -38,11 +38,14 @@
                             <div class="pl3 pr1">      
                                 <img src="@/assets/img/friends.jpg" class="grow pointer shadow-2 white br3 h4"  />
                             </div>
+                            <div class="pl3 pr1">      
+                                <img src="@/assets/img/friends.jpg" class="grow pointer shadow-2 white br3 h4"  />
+                            </div>
                         </div>
                     </div>
 
                     <div class="w-100 near-white flex flex-wrap-ns flex-nowrap " style="min-height:10rem">
-                        <div class="overflow-y-scroll flex flex-row cf w-100">
+                        <div class="overflow-y-scroll bg-near- white flex flex-row cf w-100">
                             <div class=" pb4 ">
                                 <img src="@/assets/img/building.jpg" class="grow pointer shadow-2 white br3 h4"  />
                             </div>
--- src/components/reuseables/navbar.vue
+++ src/components/reuseables/navbar.vue
@@ -1,41 +1,41 @@
 
 <template>
-    <div class="w-60 bg-black grow hover-bg-grey hover-purple fl ph2 pv2 pointer br3 shadow-5">
+    <div class="w-60-l w-40 bg-black grow hover-bg-grey hover-purple fl ph2 pv2 pointer br3 shadow-5">
         <div class="w-100  pv2-l fl tc">
-            <img src="@/assets/img/lof.png"  class="cf white h3" />
+            <img src="@/assets/img/lof.png"  class="cf  white h3" />
         </div>
-        <div class="fl w-100">
+        <div class="fl w-100-l w-50 ">
             <div class="tc pt5 ">
                 <router-link class="link white" to="/"> 
-                    <i class="f3 fas fa-home" aria-hidden="true"/> 
+                    <i class="f3 grow dim fas fa-home" aria-hidden="true"/> 
                 </router-link>
             </div>
         </div>
         <div class="fl w-100">
             <div class="tc pt5 ">
                 <router-link class="link white" to="/"> 
-                    <i class="f3 fas fa-user" aria-hidden="true"/> 
+                    <i class="f3 grow dim fas fa-user" aria-hidden="true"/> 
                 </router-link>
             </div>
         </div>
         <div class="fl w-100">
             <div class="tc pt5 ">
                 <router-link class="link white" to="/"> 
-                    <i class="f3 fas fa-envelope" aria-hidden="true"/> 
+                    <i class="f3 grow dim fas fa-envelope" aria-hidden="true"/> 
                 </router-link>
             </div>
         </div>
         <div class="fl w-100">
             <div class="tc pt5 ">
                 <router-link class="link white" to="/"> 
-                    <i class="f3 fas fa-cog" aria-hidden="true"/> 
+                    <i class="f3 grow dim fas fa-cog" aria-hidden="true"/> 
                 </router-link>
             </div>
         </div>
         <div class="fl w-100">
             <div class="tc pt6 pb4 ">
                 <router-link class="link white" to="/"> 
-                    <i class="f3 fas fa-sign-out" aria-hidden="true"/> 
+                    <i class="f3 grow dim fas fa-sign-out" aria-hidden="true"/> 
                 </router-link>
             </div>
         </div>
