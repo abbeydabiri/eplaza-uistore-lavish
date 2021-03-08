9f5b27dc7f7df7c0fabb419a581203d7e755f8ad
Abbey Dabiri <savarb@gmail.com>
2021-03-08 12:01:00 +0100

    cleaned up routes

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,6 @@ import Vue from "vue";
 import Router from "vue-router";
 Vue.use(Router);
 
-HTTP.get("/api/onlinestore").then((response) => {
-    var recVar = (response.data.Body == null) ? {} : response.data.Body;
-
-
-}).catch((e) => {
-    console.log(e)
-})
 
 
 export const router = new Router({
